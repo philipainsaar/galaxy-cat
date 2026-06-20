a23c2dc7c3a0dc80df7ee1084213bacb97d7f876
philipainsaar <82302766+philipainsaar@users.noreply.github.com>
2026-06-21 01:24:02 +0200

    Update metadata title and description

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'Cosmic Voyage',
-  description: 'Drag the alien cat into the cosmic boat.'
+  title: '𝗔𝗟𝗠𝗢𝗦𝗧 𝗠𝗔𝗗𝗘 𝗜𝗡 𝗝𝗔𝗣𝗔𝗡',
+  description: 'Place the alien cat into the boat.'
 };
 
 export default function RootLayout({ children }) {
