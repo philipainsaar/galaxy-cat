724fa9adf30627c7b49983a6165826f98769e645
philipainsaar <82302766+philipainsaar@users.noreply.github.com>
2026-06-21 01:36:52 +0200

    Add icon metadata for the application

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,16 @@ import './globals.css';
 export const metadata = {
   title: '𝗔𝗹𝗺𝗼𝘀𝘁 𝗺𝗮𝗱𝗲 𝗶𝗻 𝗝𝗮𝗽𝗮𝗻',
   description: 'Place the alien cat into the boat.'
+  icon: [
+      {
+        url: '/favicon.png',
+        sizes: 'any',
+      },
+      {
+        url: '/favicon.gif',
+        type: 'image/gif',
+      },
+    ],
 };
 
 export default function RootLayout({ children }) {
